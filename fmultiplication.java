fdbd7d8f96c7250e1dd2ef79137b8498bb59dc9d
Progga-Roy <76958372+Progga-Roy@users.noreply.github.com>
2021-06-05 13:33:21 +0600

    Add files via upload
    Multiplication done

---
+++ fmultiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class fmultiplication {
+    public static int mul(int a, int b){
+        int m=a*b;
+        return m;
+
+    }
+    public static void main(String[] args) {
+        int m;
+        m=mul(3,5);
+        System.out.print(m);
+    }
+}
